89b10a2cafb6f89d08003fb35db18ddeaa0eb203
bwkam <userdev987@gmail.com>
2023-11-05 23:36:23 +0200

    latest-kinda broken

--- src/excel_merge.rs
+++ src/excel_merge.rs
@@ -1,45 +1,15 @@
-#![allow(unused)]
+use std::{collections::HashMap, io::Cursor, sync::Arc};
 
-use std::{
-    collections::HashMap,
-    fs::File,
-    io::{BufReader, Cursor},
-    ops::Add,
-    sync::Arc,
-};
-
-use anyhow::{Error, Result};
+use anyhow::Result;
 use axum::{
-    body::Bytes,
-    extract::{Multipart, State},
+    extract::Multipart,
     http::StatusCode,
-    response::{Html, IntoResponse, Response},
-    Json,
+    response::{IntoResponse, Response},
 };
-use calamine::{open_workbook, open_workbook_from_rs, DataType, Range, Reader, Rows, Xlsx};
+use calamine::{open_workbook_from_rs, DataType, Reader, Xlsx};
+use chrono::{DateTime, NaiveDateTime, Utc};
 use rust_xlsxwriter::Workbook;
-use serde::Deserialize;
-use serde::Serialize;
-use strum_macros::Display;
 use utoipa::OpenApi;
-
-use crate::AppState;
-// #[derive(Deserialize, Debug)]
-// pub struct Data {
-//     data: Vec<Vec<serde_json::Map<String, serde_json::Value>>>, // fix this
-// }
-
-#[derive(Deserialize, Debug)]
-pub struct Data {
-    data: Vec<Vec<serde_json::Map<String, serde_json::Value>>>,
-}
-
-#[derive(Serialize, Debug)]
-struct Message {
-    message: String,
-}
-
-type MyRows<'a> = Vec<calamine::Rows<'a, DataType>>;
 
 // Make our own error that wraps `anyhow::Error`.
 pub struct AppError(anyhow::Error);
@@ -70,37 +40,14 @@ where
 #[openapi(paths(merge_files))]
 pub struct ApiDoc;
 
-#[utoipa::path(
-    post,
-    path = "/add",
-    responses(
-        (status = 200, description = "Add excel data to merge")
-    )
-)]
-pub async fn add_file(
-    State(state): State<AppState>,
-    Json(data): Json<Data>,
-) -> Result<impl IntoResponse, AppError> {
-    let mut d = state.data.lock().unwrap();
-    data.data.iter().for_each(|x| d.push(x.clone()));
-
-    println!("->> Data added to merge");
-
-    let msg = Message {
-        message: "Data added to merge".to_string(),
-    };
-
-    Ok(Json(msg))
-}
-
 #[derive(Clone, Debug)]
 pub struct FileData {
-    last_modified: u32,
+    last_modified: String,
     data: Vec<Vec<DataType>>,
 }
 
 impl FileData {
-    pub fn new(lm: u32, data: Vec<Vec<DataType>>) -> Self {
+    pub fn new(lm: String, data: Vec<Vec<DataType>>) -> Self {
         FileData {
             last_modified: lm,
             data,
@@ -117,54 +64,71 @@ impl FileData {
 )]
 pub async fn merge_files(mut multipart: Multipart) -> Result<impl IntoResponse, AppError> {
     let mut rows_hash: HashMap<String, FileData> = HashMap::new();
-    let mut first_row_headers: Vec<String> = Vec::new();
     let mut cutting_rows: u32 = 0;
 
-    while let Some(mut field) = multipart.next_field().await.unwrap() {
-        println!("In the loop here.");
+    while let Some(field) = multipart.next_field().await.unwrap() {
         let content_type = field.content_type().map(str::to_owned);
 
         let name = field.name().unwrap_or("unknown").to_owned();
         let other_name = field.file_name().unwrap_or("unknown").to_owned();
         let bytes = field.bytes().await.unwrap();
 
-        if name == "cuttingRows" {
-            println!("Setting cutting rows.");
+        if name.ends_with("LM") {
+            let last_modified_str = String::from_utf8(bytes.to_vec()).unwrap();
+            let last_modified_i64 = last_modified_str.parse::<i64>().unwrap();
 
+            let naive = NaiveDateTime::from_timestamp_millis(last_modified_i64).unwrap();
+            let datetime: DateTime<Utc> = DateTime::from_naive_utc_and_offset(naive, Utc);
+
+            let date = format!("{}", datetime.format("%Y %m %d %H%M"));
+
+            let file_name = name.split("-").next().unwrap().to_string();
+
+            if rows_hash.contains_key(&file_name) {
+                let val = rows_hash.get_mut(&file_name).unwrap();
+                val.last_modified = date;
+            } else {
+                rows_hash.insert(file_name.to_owned(), FileData::new(date, Vec::new()));
+            }
+        }
+
+        if name == "cuttingRows" {
             let cutting_rows_str = String::from_utf8(bytes.to_vec()).unwrap();
 
             if !cutting_rows_str.is_empty() {
-                println!("Cutting rows: {:?}", cutting_rows);
                 cutting_rows = cutting_rows_str.parse::<u32>().unwrap();
             }
         }
 
-        println!("Final Cutting rows: {:?}", cutting_rows);
-
         if content_type
             == Some("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet".to_string())
         {
-            let bytes = bytes.to_vec();
-            let reader = Cursor::new(bytes);
-            let mut workbook: Xlsx<_> = open_workbook_from_rs(reader).unwrap();
-            if let Some(range) = workbook.worksheet_range_at(0) {
-                let sheet = range.unwrap();
-                let rows: Vec<_> = sheet
-                    .to_owned()
-                    .rows()
-                    .into_iter()
-                    .map(|slice| slice.to_vec())
-                    .collect();
+            let task = tokio::task::spawn_blocking(move || {
+                let bytes = bytes.to_vec();
+                let reader = Cursor::new(bytes);
+                let mut workbook: Xlsx<_> = open_workbook_from_rs(reader).unwrap();
+                if let Some(range) = workbook.worksheet_range_at(0) {
+                    let sheet = range.unwrap();
+                    let rows: Vec<_> = sheet
+                        .to_owned()
+                        .rows()
+                        .into_iter()
+                        .map(|slice| slice.to_vec())
+                        .collect();
 
-                // rows_hash.insert(other_name.to_owned(), rows);
-
-                if rows_hash.contains_key(&other_name) {
-                    let mut val = rows_hash.get_mut(&other_name).unwrap();
-                    val.data = rows;
-                } else {
-                    rows_hash.insert(other_name.to_owned(), FileData::new(0, rows));
+                    if rows_hash.contains_key(&other_name) {
+                        let val = rows_hash.get_mut(&other_name).unwrap();
+                        val.data = rows;
+                    } else {
+                        rows_hash.insert(
+                            other_name.to_owned(),
+                            FileData::new("unkown".to_string(), rows),
+                        );
+                    }
                 }
-            }
+            });
+
+            task.await.unwrap();
         }
     }
 
@@ -175,40 +139,38 @@ pub async fn merge_files(mut multipart: Multipart) -> Result<impl IntoResponse, 
 
     let mut first_rows: Vec<String> = rows_hash_clone_locked
         .iter()
-        .map(|(_, inner_vec)| {
-            let first_row = inner_vec.first().unwrap();
-            first_row
-                .iter()
-                .map(|rowData| match rowData {
-                    DataType::String(s) => s.to_string(),
-                    DataType::Float(f) => f.to_string(),
-                    DataType::Int(i) => i.to_string(),
-                    DataType::Bool(b) => b.to_string(),
-                    DataType::Error(e) => e.to_string(),
-                    DataType::Empty => "".to_string(),
-                    _ => "".to_string(),
-                })
-                .collect::<Vec<String>>()
+        .next()
+        .unwrap()
+        .1
+        .data
+        .first()
+        .unwrap()
+        .iter()
+        .map(|data| match data {
+            DataType::String(s) => s.to_string(),
+            DataType::Float(f) => f.to_string(),
+            DataType::Int(i) => i.to_string(),
+            DataType::Bool(b) => b.to_string(),
+            DataType::Error(e) => e.to_string(),
+            DataType::Empty => "".to_string(),
+            _ => "".to_string(),
         })
-        .flatten()
         .collect();
 
-    println!("First rows: {:?}", first_rows);
-
-    // TODO: see if flat_map works instead of flatten
     let mut values_rows: Vec<Vec<String>> = rows_hash
         .into_iter()
         .enumerate()
         .map(|(i, (name, inner_vec))| {
-            let mut main_data = inner_vec
+            let main_data = inner_vec
+                .data
                 .iter()
-                .skip((1 + cutting_rows) as usize) // skip the first row of the inner vector
+                .skip((1 + cutting_rows) as usize)
                 .enumerate()
                 .map(|(j, file)| {
                     let mut intro_headers = vec![];
                     let mut cur_row_values: Vec<String> = file
                         .iter()
-                        .map(|rowData| match rowData {
+                        .map(|row_data| match row_data {
                             DataType::String(s) => s.to_string(),
                             DataType::Float(f) => f.to_string(),
                             DataType::Int(i) => i.to_string(),
@@ -219,26 +181,16 @@ pub async fn merge_files(mut multipart: Multipart) -> Result<impl IntoResponse, 
                         })
                         .collect();
 
-                    intro_headers.push("2023".to_string());
+                    intro_headers.push((&inner_vec.last_modified).to_owned());
                     intro_headers.push(i.to_string());
                     intro_headers.push(j.to_string());
                     intro_headers.push(j.to_string() + "-" + i.to_string().as_str());
                     intro_headers.push(name.to_string());
 
                     let mut vec: Vec<String> = vec![];
-                    println!("Intro headers: {:?}", intro_headers);
                     vec.append(&mut intro_headers);
-                    // print intro headers
-
-                    // print the main data
-                    println!("Main row data: {:?}", cur_row_values);
                     vec.append(&mut cur_row_values);
-
                     vec
-
-                    // cur_row_values
-                    // intro_headers.append(&mut cur_row_values);
-                    // intro_headers
                 })
                 .collect::<Vec<Vec<String>>>();
 
@@ -246,9 +198,6 @@ pub async fn merge_files(mut multipart: Multipart) -> Result<impl IntoResponse, 
         })
         .flatten()
         .collect();
-
-    // print values in rows
-    // values_rows.iter().for_each(|x| println!("{:?}", x));
 
     // modify the headers
     let mut extra_headers = vec![
@@ -264,15 +213,14 @@ pub async fn merge_files(mut multipart: Multipart) -> Result<impl IntoResponse, 
 
     extra_headers.append(&mut first_rows);
 
-    // println!("Extra headers: {:?}", extra_headers);
-
     values_rows.insert(0, extra_headers);
 
+    // TODO: put this in a seperate thread
     let mut workbook = Workbook::new();
     let worksheet = workbook.add_worksheet();
 
-    worksheet.write_row_matrix(0, 0, &values_rows);
+    worksheet.write_row_matrix(0, 0, &values_rows)?;
     workbook.save("output.xlsx").unwrap();
 
-    Ok("Success".to_string())
+    Ok(workbook.save_to_buffer().unwrap().to_vec())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,24 @@
-#![allow(unused)]
-
 use axum::{
-    body::Bytes,
     extract::DefaultBodyLimit,
     http::{
         header::{ACCEPT, AUTHORIZATION, CONTENT_TYPE, LAST_MODIFIED},
         HeaderValue, Method,
     },
-    response::{Html, IntoResponse},
-    routing::{get, post},
-    Json, Router,
+    routing::post,
+    Router,
 };
-use calamine::{open_workbook_auto_from_rs, open_workbook_from_rs, Reader, Sheets, Xlsx};
-use excel_merge::{ApiDoc, Data};
-use serde::Deserialize;
-use std::{
-    io::{BufReader, Cursor},
-    net::SocketAddr,
-    sync::{Arc, Mutex},
-};
+use excel_merge::ApiDoc;
+use std::net::SocketAddr;
 use tower_http::cors::CorsLayer;
 use utoipa::OpenApi;
 use utoipa_swagger_ui::SwaggerUi;
 pub mod error;
 pub mod excel_merge;
 
-#[derive(Clone)]
-pub struct AppState {
-    data: Arc<Mutex<Vec<Vec<serde_json::Map<String, serde_json::Value>>>>>,
-}
-
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let state = AppState {
-        data: Arc::new(Mutex::new(Vec::new())),
-    };
-
     let router = Router::new()
-        .route("/hello", post(hello))
         .route("/merge", post(excel_merge::merge_files))
-        .route("/add", post(excel_merge::add_file))
-        .with_state(state)
         .merge(SwaggerUi::new("/swagger-ui").url("/api-doc/openapi.json", ApiDoc::openapi()))
         .layer(
             CorsLayer::new()
@@ -58,15 +36,4 @@ async fn main() -> anyhow::Result<()> {
         .unwrap();
 
     Ok(())
-}
-
-#[derive(Deserialize, Debug)]
-pub struct Payload {
-    data: Vec<String>,
-}
-
-async fn hello(body: Bytes) -> impl IntoResponse {
-    let bytes = body;
-    let reader = Cursor::new(&bytes);
-    let mut excel: Xlsx<_> = open_workbook_from_rs(reader).unwrap();
 }
